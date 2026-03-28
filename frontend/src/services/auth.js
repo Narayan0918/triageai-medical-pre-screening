bc414a8c95a59fbabd8ecda39efcac9fda7ee73b
Narayan <narayanrajdubey9889@gmail.com>
2026-03-28 16:54:54 +0530

    updated the backend api for frontend

--- frontend/src/services/auth.js
+++ frontend/src/services/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://triage-ai-api-wiu9.onrender.com/api/';
+const BASE_URL = 'https://triage-ai-api-2vlf.onrender.com/api/';
 
 export const login = async (username, password) => {
     const response = await axios.post(`${BASE_URL}token/`, { username, password });
